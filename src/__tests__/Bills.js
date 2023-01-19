4167312e8661fb7ab8041b22c7e61defd3594a2a
Simon Tessard <simon.tessard@gmail.com>
2023-01-19 21:55:49 +0100

    Added Expect reference

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -25,10 +25,10 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
-
+      expect(windowIcon.className).toBe('active-icon')
     })
     test("Then bills should be ordered from earliest to latest", () => {
+      
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
