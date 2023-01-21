96565e2ec3d330bd65e8ba3ab34ba06b870a09aa
Simon Tessard <simon.tessard@gmail.com>
2023-01-21 18:51:08 +0100

    Added loading page test, content test and new bill click test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,16 +2,53 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import { screen, waitFor } from "@testing-library/dom"
+import userEvent from '@testing-library/user-event'
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import Bills from "../containers/Bills.js"
 
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
+  describe('When I am on Bills page but it is loading', () => {
+    test('Then, Loading page should be rendered', () => {
+
+      document.body.innerHTML = BillsUI({ loading: true })
+      expect(screen.getAllByText('Loading...')).toBeTruthy()
+    })
+  })
   describe("When I am on Bills Page", () => {
+    test("The page should not be empty", async () => {
+
+      document.body.innerHTML = BillsUI({ data: bills })
+      const body = await screen.getByTestId('tbody')
+      expect(body).toBeDefined()
+    })
+    describe('When I click on the new bill button', () => {
+      test('I must be redirected to New Bill Page', () => {
+
+        document.body.innerHTML = BillsUI({ data: bills })
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const store = null
+        const bill = new Bills({
+          document, onNavigate, store, localStorage: window.localStorage
+        })
+
+        const handleClickButton = jest.fn(bill.handleClickNewBill)
+        const newBillButton = screen.getByTestId('btn-new-bill')
+        newBillButton.addEventListener('click', handleClickButton)
+        userEvent.click(newBillButton)
+        expect(handleClickButton).toHaveBeenCalled()
+        expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy()
+      })
+    })
+
     test("Then bill icon in vertical layout should be highlighted", async () => {
 
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
@@ -28,7 +65,7 @@ describe("Given I am connected as an employee", () => {
       expect(windowIcon.className).toBe('active-icon')
     })
     test("Then bills should be ordered from earliest to latest", () => {
-      
+
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
