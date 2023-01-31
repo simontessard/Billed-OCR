5674fe1de6416c6ed671d245180492fe7fd8123d
Simon Tessard <simon.tessard@gmail.com>
2023-01-31 23:04:59 +0100

    ID given to error message to prevent others elements conflicts

--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -56,7 +56,7 @@ export default () => {
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
                     <input required type="file" class="form-control blue-border" data-testid="file" />
-                    <span data-testid="fileErrorMsg"> </span>
+                    <span data-testid="fileErrorMsg" id="fileErrorMsg"> </span>
                   </div>
                 </div>
             </div>
